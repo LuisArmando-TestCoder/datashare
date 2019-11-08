5babb7e76b06c298cb1271d35672204e1acdffff
LuisArmando-TestCoder <luisarmando.murillobaltodano@gmail.com>
2019-11-07 22:06:41 -0600

    test cors

--- src/api.js
+++ src/api.js
@@ -9,11 +9,15 @@ router.get('/newer-entries/:amount', (req, res) => {
     const newerEntries = Object.keys(entries)
         .map(key => ({ title: key, entry: entries[key] }))
         .splice(0, amount);
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
     res.json({ success: true, data: newerEntries });
 });
 
 router.get('/entry/:title', (req, res) => {
     const { title } = req.params;
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
     res.json({ success: true, data: entries[title] });
 });
 
@@ -21,13 +25,9 @@ router.post('/entry', (req, res) => {
     const { title, entry } = req.body;
     if (entries[title]) return res.json({ success: false, message: 'There is an entry with that title already' });
     entries[title] = entry;
-    return res.json({ success: true, message: `${title} has been successfully added to entries` });
-});
-
-app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "*");
-    next();
+    return res.json({ success: true, message: `${title} has been successfully added to entries` });
 });
 app.use(express.urlencoded({ extended: false }));
 app.use('/.netlify/functions/api', router);
