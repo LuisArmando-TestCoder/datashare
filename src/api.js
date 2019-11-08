30454c814ea17bc9fe180af810d9aaf340c1d157
LuisArmando-TestCoder <luisarmando.murillobaltodano@gmail.com>
2019-11-07 21:49:09 -0600

    add: sharedata mvp

---
+++ src/api.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const serverless = require('serverless-http');
+const app = express();
+const router = express.Router();
+const entries = {};
+
+router.get('/newer-entries/:amount', (req, res) => {
+    const { amount } = req.params;
+    const newerEntries = Object.keys(entries)
+        .map(key => ({ title: key, entry: entries[key] }))
+        .splice(0, amount);
+    res.json({ success: true, data: newerEntries });
+});
+
+router.get('/entry/:title', (req, res) => {
+    const { title } = req.params;
+    res.json({ success: true, data: entries[title] });
+});
+
+router.post('/entry', (req, res) => {
+    const { title, entry } = req.body;
+    if (entries[title]) return res.json({ success: false, message: 'There is an entry with that title already' });
+    entries[title] = entry;
+    return res.json({ success: true, message: `${title} has been successfully added to entries` });
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.use('/.netlify/functions/api', router);
+
+module.exports.handler = serverless(app);
